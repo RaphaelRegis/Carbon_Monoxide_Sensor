114341a0b4e721aa5eaf212a01c16cb525afe2f6
RaphaelRegis <raph4life17@gmail.com>
2025-07-04 17:48:47 -0300

    feat: Telegram integration

--- AWS_Lambda/Lambda_Handler.py
+++ AWS_Lambda/Lambda_Handler.py
@@ -1,7 +1,13 @@
+import json
 import os
+import urllib
+import urllib.parse
+import urllib.request
 
 def collect_request_body(event):
-    pass
+    body_str = event.get('body', '{}')
+    body = json.loads(body_str)
+    return body.get('token'), body.get('measurement')
 
 def verify_token(token):
     return token == os.environ("VALIDY_TOKEN")
@@ -9,10 +15,23 @@ def verify_token(token):
 def classify_measurement(measurement):
     pass
 
-def send_message(message):
-    pass
-
-
+def send_Telegram_message(message):
+    TELEGRAM_BOT_TOKEN = os.environ("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_CHAT_ID = os.environ("TELEGRAM_CHAT_ID")
+    
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    
+    data = urllib.parse.urlencode({
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }).encode("utf-8")
+    
+    req= urllib.request.Request(url, data=data)
+    
+    with urllib.request.urlopen(req) as response:
+        return json.loads(response.read())
+    
 def lambda_handler(event, context):
     # coleta o corpo da requisicao
     token, measurement = collect_request_body(event)    
@@ -30,9 +49,9 @@ def lambda_handler(event, context):
     message = classify_measurement(measurement)
     
     # envia a mensagem para o Telegram
-    send_message(message)
+    result = send_Telegram_message(message)
     
     return {
         "statusCode": 200,
-        "message": "Measurement processed sucessfully"
+        "message": json.dumps(result)
     }
