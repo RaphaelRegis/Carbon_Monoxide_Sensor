705ac449c908e3097cf97c6cce5ce7c2fab62d72
RaphaelRegis <raph4life17@gmail.com>
2025-07-05 00:00:27 -0300

    feat: test implementation with mocked event

--- AWS_Lambda/Lambda_Handler.py
+++ AWS_Lambda/Lambda_Handler.py
@@ -1,24 +1,41 @@
 import json
 import os
+from dotenv import load_dotenv
 import urllib
 import urllib.parse
 import urllib.request
 
+# load environment variables
+load_dotenv()
+VALIDY_TOKEN = os.getenv("VALIDY_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
 def collect_request_body(event):
-    body_str = event.get('body', '{}')
-    body = json.loads(body_str)
-    return body.get('token'), body.get('measurement')
+    body = json.loads(event['body'])
+    return body['token'], body['measurement']
 
 def verify_token(token):
-    return token == os.environ("VALIDY_TOKEN")
+    return token == VALIDY_TOKEN
 
 def classify_measurement(measurement):
-    pass
+    
+    # REAJUSTAR DE ACORDO COM FONTES CONFIÁVEIS
+    measurement = int(measurement)
+    
+    classificacao = ""
+    if measurement < 9:
+        classificacao = "Ar bom"
+    elif measurement >= 9 and measurement <= 10:
+        classificacao = "Ar moderado"
+    elif measurement >= 11 and measurement <= 50:
+        classificacao = "Ar ruim"
+    elif measurement > 50:
+        measurement = "Ar péssimo"
+        
+    return classificacao
 
 def send_Telegram_message(message):
-    TELEGRAM_BOT_TOKEN = os.environ("TELEGRAM_BOT_TOKEN")
-    TELEGRAM_CHAT_ID = os.environ("TELEGRAM_CHAT_ID")
-    
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     
     data = urllib.parse.urlencode({
@@ -33,13 +50,13 @@ def send_Telegram_message(message):
         return json.loads(response.read())
     
 def lambda_handler(event, context):
-    # coleta o corpo da requisicao
+    # collect request body variables
     token, measurement = collect_request_body(event)    
     
     #verifica a credencial na requisicao
     validity = verify_token(token)
     
-    if not validity:
+    if validity == False:
         return {
             "statusCode": 401,
             "message": "Invalid token"
@@ -49,9 +66,21 @@ def lambda_handler(event, context):
     message = classify_measurement(measurement)
     
     # envia a mensagem para o Telegram
-    result = send_Telegram_message(message)
-    
+    #result = send_Telegram_message(message)
+    result = {
+        "Token": token,
+        "Measurement": measurement,
+        "Message": message
+    }
     return {
         "statusCode": 200,
         "message": json.dumps(result)
     }
+    
+# para realizar testes localmente
+if __name__ == "__main__":
+    with open("AWS_Lambda\event.json") as f:
+        event = json.load(f)    
+    
+    print(lambda_handler(event, None))
+    
